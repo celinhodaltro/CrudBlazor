a318c65dc97a22caa508bf57781821096112a517
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-07-11 18:00:55 -0300

    Criação de adição de produtos

--- CrudBlazor/CrudBlazor/Program.cs
+++ CrudBlazor/CrudBlazor/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -20,7 +20,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
