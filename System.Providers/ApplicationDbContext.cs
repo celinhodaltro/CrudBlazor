b1c1bdb2e5ba47f9edfd70739a31a7a2fb4b311f
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-07-11 10:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ System.Providers/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Entities;
+using System.Security.Principal;
+
+public class ApplicationDbContext : DbContext
+{
+    public string ConnectionString { get; set; } = "Server=localhost;Database=Main;Uid=root;Pwd=admin";
+
+
+    public ApplicationDbContext() { }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySQL(ConnectionString);
+        }
+    }
+
+
+
+
+}
+
+
+
